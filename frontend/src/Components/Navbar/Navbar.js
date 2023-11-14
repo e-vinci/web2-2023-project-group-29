11e9b891bf6959fa55b92475e4ef0245845f4105
Koubai <omar.koubai@student.vinci.be>
2023-11-14 18:23:31 +0100

    adding: game page in the navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -35,7 +35,10 @@ const Navbar = () => {
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#" data-uri="/register">Register Page</a>
-              </li>                        
+              </li>  
+              <li class="nav-item">
+                <a class="nav-link" href="#" data-uri="/game">game Page</a>
+              </li>                       
             </ul>
           </div>
         </div>
--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,13 @@
 import HomePage from '../Pages/HomePage';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
+import GamePage  from '../Pages/GamePage';
 
 const routes = {
   '/': HomePage,
   '/login': LoginPage,
   '/register': RegisterPage, 
+  '/game':GamePage
 };
 
 export default routes;
