54ee206b262a261d99183c2fe35214a287e0e6d2
Koubai <omar.koubai@student.vinci.be>
2023-11-15 19:47:15 +0100

    Adding the memory timer functionality

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -1,51 +1,67 @@
 // eslint-disable-next-line
 import anime from 'animejs';
 
+const memoryTimer = 5;
 
-
-const GamePage = () => {
-
-    /*  
+const handleCardClick = (card) => {
+  /*  
     Author-> Joshua McFarland -> https://codesandbox.io/u/mcfarljw 
     URL of the Code: https://codesandbox.io/u/mcfarljw
-    */
-    const body=document.querySelector("body");
-    
-    body.innerHTML=`<div class="card-container">
-                      <div class="card">
-                        <div class="front">
-                          A
-                        </div>
-                        <div class="back">
-                          B
-                        </div>
-                      </div>
-                    </div>`;
-    const card = document.querySelector(".card");
+    ligne 14 -> 21
+  */
 
-    let playing = false;
-    
+  anime({
+    targets: card,
+    scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+    rotateY: { value: '+=540', delay: 200 },
+    easing: 'easeInOutSine',
+    duration: 400,
+  });
+};
 
-    card.addEventListener("click", () => {
-      if (playing) return;
+const GamePage = () => {
+  /*  
+    Author-> Joshua McFarland -> https://codesandbox.io/u/mcfarljw 
+    URL of the Code: https://codesandbox.io/u/mcfarljw
+    ligne 34 -> 45
+  */
 
-      playing = true;
+  let innerHTML = `<div class="card-container">`;
+  // eslint-disable-next-line no-plusplus
+  for (let index = 0; index < 4; index++) {
+    innerHTML += `<div class="card">
+                    <div class="front">
+                        ${index}
+                    </div>
+                    <div class="back">
+                        ${index + 1}
+                    </div>                  
+                  </div>`;
+  }
 
-      anime({
-        targets: card,
-        scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
-        rotateY: { value: "+=540", delay: 200 },
-        easing: "easeInOutSine",
-        duration: 400,
-        
-        // eslint-disable-next-line no-unused-vars
-        complete: (anim) => {
-          playing = false;
-        }
-      });
+  const main = document.querySelector('main');
+  main.innerHTML = `${innerHTML} </div>`;
+
+  const cards = document.querySelectorAll('.card');
+
+  // Retourne toutes les cartes des le debut de la partie afin que le joueur puise memoriser les cartes dans les temps impartie
+  cards.forEach((card) => {
+    handleCardClick(card);
+  });
+
+  // Retourner toutes les cartes après que le memoryTimer expirer
+  setTimeout(() => {
+    cards.forEach((card) => {
+      handleCardClick(card);
     });
+  }, memoryTimer * 1000);
 
+  // Mise en place d'ecouteur d'evenement sur toutes les cartes lorsque que l'on click sur une carte. Cela la retourne .
+  cards.forEach((card) =>
+    card.addEventListener('click', () => {
+      handleCardClick(card);
+    }),
+  );
 };
 
 export default GamePage;
-
