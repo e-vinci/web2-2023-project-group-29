0a781d6688fbefdee19655a03a8949b3f287d1a2
dawid-ciborowski-vinci <dawid.ciborowski@student.vinci.be>
2023-11-18 02:07:13 +0100

    update : missing files

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -1,8 +1,9 @@
 const LoginPage = () => {
-    const main = document.querySelector('main');
-  
-    const loginPage = `
-      <div class="container mt-5" style="background-color: black; color: white;">
+  const main = document.querySelector('main');
+
+  const loginPage = `
+  <div class="full-screen-bg">
+      <div class="container mt-5">
         <div class="row justify-content-center">
           <div class="col-md-6">
             <form id="loginForm">
@@ -17,20 +18,20 @@ const LoginPage = () => {
               </div>
   
               <br>
-              <button type="submit" class="btn btn-warning btn-block">Se connecter</button>
+              <button type="submit" class="btn btn-warning btn-block" data-uri="/game">Se connecter</button>
               
               <p class="mt-3 text-center">
                 Vous n'avez pas de compte ? 
-                <a href="/inscription" class="text-warning">S'inscrire ici</a>
+                <a href="/inscription" class="text-warning" data-uri="/register">S'inscrire ici</a>
               </p>
             </form>
           </div>
         </div>
       </div>
+    </div>
     `;
-  
-    main.innerHTML = loginPage;
-  };
-  
-  export default LoginPage;
-  
+
+  main.innerHTML = loginPage;
+};
+
+export default LoginPage;
