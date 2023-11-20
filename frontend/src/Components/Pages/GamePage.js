e674ab046d5c95c5f8204c380a197cc8646a6091
Koubai <omar.koubai@student.vinci.be>
2023-11-20 11:59:51 +0100

    updating the branch memoryTimer

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -5,18 +5,20 @@ import anime from 'animejs';
 import levels from '../../../../data/level.json';
 import { clearPage } from '../../utils/render';
 
-const cardNumber = levels[0].card_number/2
-const bossLifeMax = cardNumber*10;
+const cardNumber = levels[0].card_number / 2;
+const bossLifeMax = cardNumber * 10;
 let bossLife = bossLifeMax;
 const memoryTimer = 5;
+let clickable = false;
+
 
 const main = document.querySelector('main');
 
-function GamePage () {
-  clearPage()
-  
+function GamePage() {
+  clearPage();
+
   // affichage de la barre de vie du boss
-  
+
   displayBossLife();
   // Ajout de la div pour afficher le minuteur
   buildGamePage();
@@ -31,27 +33,27 @@ function GamePage () {
   turnAllTheCards();
 
   // Mise en place d'un écouteur d'événement sur toutes les cartes lorsque l'on clique sur une carte. Cela la retourne.
-  cards.forEach((card) =>
+
+  cards.forEach((card) => {
     card.addEventListener('click', () => {
-      handleCardClick(card);
-      bossLife-=10;
-      bossLifeWrapper.innerText = bossLife
-      animationBossLife(lifeBarWrapper);
-    }),
-  );
+      if (clickable === true) {
+        bossLife -= 10;
+        bossLifeWrapper.innerText = bossLife;
+        animationBossLife(lifeBarWrapper);
+        handleCardClick(card); 
+      }
+    });
+  });
+}
 
-  
-};
-
-function displayBossLife(){
+function displayBossLife() {
   const div = document.createElement('div');
-  div.className = 'container text-center'
+  div.className = 'container text-center';
   const divrow = document.createElement('div');
-  divrow.className = 'row align-items-center'
-  const divLife  = document.createElement('div');
+  divrow.className = 'row align-items-center';
+  const divLife = document.createElement('div');
   divLife.id = 'life';
   main.appendChild(divLife);
-
 
   const divBossLife = document.createElement('div');
   divBossLife.id = 'LifeBar';
@@ -62,19 +64,16 @@ function displayBossLife(){
   p.id = 'bossLife';
   p.className = 'text-center';
   divBossLife.appendChild(p);
-  
 }
 
 function buildGamePage() {
   let innerHTML = `<div id="memoryTimer"></div> 
                   <div class="card-container">`;
 
-
   for (let index = 0; index < cardNumber; index++) {
     innerHTML += `<div class="card">
                   <div class="front">
-                      ?
-                  </div>
+                      ?                  </div>
                   <div class="back">
                       ${index + 1}
                   </div>                  
@@ -83,20 +82,22 @@ function buildGamePage() {
   main.innerHTML += `${innerHTML} </div>`;
 }
 
-
 function createTimer(timer) {
   let secondsRemaining = timer;
-  
+
   // Mise en place du minuteur avec setTimeout
   const timerInterval = setInterval(() => {
     if (document.getElementById('memoryTimer') !== null) {
       // Mise à jour du texte du minuteur
-      document.getElementById('memoryTimer').innerText = `Temps restant : ${secondsRemaining} secondes`;
+      document.getElementById(
+        'memoryTimer',
+      ).innerText = `Temps restant : ${secondsRemaining} secondes`;
 
       // Si les secondes restante sont a 0 on efface la div memoryTimer
       if (secondsRemaining === 0) {
         clearInterval(timerInterval);
-        document.getElementById('memoryTimer').innerHTML = '';
+        document.getElementById('memoryTimer').innerHTML = null;
+        clickable = true;
 
         // Retourner toutes les cartes après que le minuteur a expiré
         turnAllTheCards();
@@ -105,51 +106,44 @@ function createTimer(timer) {
     } else {
       clearInterval(timerInterval);
     }
-  }, timer * 100); // Appel de la fonction toutes les 1000 millisecondes (1 seconde)
+  }, 1000); // Appel de la fonction toutes les 1000 millisecondes (1 seconde)
 }
 
-function turnAllTheCards(){
-  
-  const cards = document.querySelectorAll(".card");
+function turnAllTheCards() {
+  const cards = document.querySelectorAll('.card');
   cards.forEach((card) => {
     handleCardClick(card);
   });
 }
 
-function handleCardClick (card)  {
+function handleCardClick(card) {
   /** *************************************************************************************
-*    Title: flip a card with animeJS
-*    Author: Joshua McFarland -> https://codesandbox.io/u/mcfarljw 
-*    Date: no date
-*    Code version:
-*    Availability: https://codesandbox.io/s/e7ou1
-*
-************************************************************************************** */
-
+   *    Title: flip a card with animeJS
+   *    Author: Joshua McFarland -> https://codesandbox.io/u/mcfarljw
+   *    Date: no date
+   *    Code version:
+   *    Availability: https://codesandbox.io/s/e7ou1
+   *
+   ************************************************************************************** */
 
   anime({
     targets: card,
     scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
-    rotateY: { value: '+=540', delay: 200 },
+    rotateY: { value: '+=540', delay: 100 },
     easing: 'easeInOutSine',
     duration: 400,
   });
-};
-
-
-function animationBossLife(lifeBarWrapper){
-  console.log((bossLife/bossLifeMax)*100);
-  anime({
-    targets:lifeBarWrapper,
-    width:`${(bossLife/bossLifeMax)*100}%`,
-    duration: 500,
-    easing:'easeInOutSine',
-    
-  })
+ 
 }
 
-
-
-
+function animationBossLife(lifeBarWrapper) {
+  console.log((bossLife / bossLifeMax) * 100);
+  anime({
+    targets: lifeBarWrapper,
+    width: `${(bossLife / bossLifeMax) * 100}%`,
+    duration: 500,
+    easing: 'easeInOutSine',
+  });
+}
 
 export default GamePage;
