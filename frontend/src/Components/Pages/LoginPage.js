3ceefe3218f29a406645a65877a79f020b779544
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2023-11-22 21:28:48 +0100

    add: button on login and register pages

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -10,20 +10,26 @@ const LoginPage = () => {
     <div class="full-screen-bg">
       <div class="container mt-5">
         <div class="row justify-content-center">
+          <div class="col-md-15 d-flex justify-content-end">
+            <!-- Ajout du bouton "Retour" -->
+            <button id="backButton" class="btn btn-warning ml-auto">Retour</button>
+          </div>
           <div class="col-md-6">
             <form id="loginForm">
               <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" class="form-control" id="email" name="email" required>
               </div>
-              <br>
+
               <div class="form-group">
                 <label for="password">Mot de passe</label>
                 <input type="password" class="form-control" id="password" name="password" required>
               </div>
+              
               <br>
-              <button type="submit" class="btn btn-warning btn-block">Se connecter</button>
-              <br>
+              <div class="text-center">
+                <button type="submit" class="btn btn-warning btn-block">Se connecter</button>
+              </div>
               <br>
               <p class="mt-3 text-center">
                 N'avez-vous pas de compte ? 
@@ -39,6 +45,12 @@ const LoginPage = () => {
   main.innerHTML = loginPage;
 
   const loginForm = document.querySelector('#loginForm');
+
+  // Ajout du gestionnaire d'événements pour le bouton "Retour"
+  const backButton = document.getElementById('backButton');
+  backButton.addEventListener('click', () => {
+    Navigate('/play'); // Naviguer vers la page de jeu
+  });
 
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -62,4 +74,3 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
-
--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -10,30 +10,45 @@ const RegisterPage = () => {
     <div class="full-screen-bg">
       <div class="container mt-5">
         <div class="row justify-content-center">
+          <div class="col-md-15 d-flex justify-content-end">
+            <!-- Modification du style du bouton "Back" -->
+            <button id="backButton" class="btn btn-warning ml-auto">Retour</button>
+          </div>
           <div class="col-md-6">
             <form id="registrationForm">
               <div class="form-group">
                 <label for="username">Nom d'utilisateur</label>
                 <input type="text" class="form-control" id="username" name="username" required>
               </div>
-  
+
               <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" class="form-control" id="email" name="email" required>
               </div>
-  
+
               <div class="form-group">
                 <label for="password">Mot de passe</label>
                 <input type="password" class="form-control" id="password" name="password" required>
               </div>
-  
+
               <div class="form-group">
                 <label for="confirmPassword">Confirmer le mot de passe</label>
                 <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
               </div>
-  
+
+              <div class="form-group">
+                <label for="profilePicture">Photo de profil</label>
+                <input type="file" class="form-control-file" id="profilePicture" name="profilePicture" accept="image/*" required>
+              </div>
+
+              <!-- Ajout de divs pour afficher les messages d'erreur -->
+              <div id="passwordMismatchError" class="text-danger"></div>
+              <div id="existingUserError" class="text-danger"></div>
+
               <br>
-              <button type="submit" class="btn btn-warning btn-block">S'inscrire</button>
+              <div class="text-center">
+                <button type="submit" class="btn btn-warning btn-block">S'inscrire</button>
+              </div>
             </form>
           </div>
         </div>
@@ -44,6 +59,14 @@ const RegisterPage = () => {
   main.innerHTML = registerPage;
 
   const registrationForm = document.querySelector('#registrationForm');
+  const passwordMismatchError = document.querySelector('#passwordMismatchError');
+  const existingUserError = document.querySelector('#existingUserError');
+
+  // Ajout du gestionnaire d'événements pour le bouton Back
+  const backButton = document.getElementById('backButton');
+  backButton.addEventListener('click', () => {
+    Navigate('/login'); // Naviguer vers la page de connexion
+  });
 
   registrationForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -55,15 +78,16 @@ const RegisterPage = () => {
 
     // Vérification de la correspondance des mots de passe
     if (password !== confirmPassword) {
-      alert("Les mots de passe ne correspondent pas.");
+      passwordMismatchError.textContent = "Les mots de passe ne correspondent pas.";
+      existingUserError.textContent = ""; // Effacer le message d'utilisateur existant s'il y en a un
       return;
     }
 
     // Vérification si l'utilisateur existe déjà
     const existingUser = readAllUsers().find((user) => user.email === email);
     if (existingUser) {
-      alert("Cet utilisateur existe déjà. Veuillez vous connecter.");
-      Navigate('/login');
+      existingUserError.textContent = "Cet utilisateur existe déjà. Veuillez vous connecter.";
+      passwordMismatchError.textContent = ""; // Effacer le message de mot de passe s'il y en a un
       return;
     }
 
