cf1197ab60c539aa82db9f2ef0e7cc6f58f7e533
melik <melik.budak@student.vinci.be>
2023-11-24 15:00:51 +0100

    update levelpage

--- frontend/src/Components/Pages/levelPage.js
+++ frontend/src/Components/Pages/levelPage.js
@@ -2,17 +2,19 @@ import  levels  from "../../../../data/level.json";
 import Navigate from "../Router/Navigate";
 import { clearPage } from '../../utils/render';
 
-const main = document.querySelector('main');
 
+const main = document.querySelector('main');
 function levelPage (){
     clearPage();
     buildLevelPage();  
 
-
 }
 function buildLevelPage() {
+    const div = document.createElement('div');
+    div.className = 'divMap'
+    main.appendChild(div)
     const divTitle = document.createElement('div');
-    main.appendChild(divTitle);
+    div.appendChild(divTitle);
 
     const title = document.createElement('h1');
     divTitle.appendChild(title);
@@ -21,23 +23,20 @@ function buildLevelPage() {
     
 
     const divList = document.createElement('div');
-    main.appendChild(divList);
-    divList.className = 'container'
+    div.appendChild(divList);
+    divList.className = 'levels'
     
 
 
     levels.forEach(level => {
         const rowDiv = document.createElement('div');
-    
         const rowListTitle = document.createElement('h2');
         rowDiv.appendChild(rowListTitle);
-        rowDiv.className = 'list-group-item text-center';
+        rowDiv.className = 'rowLevel';
         rowDiv.id = `${level.level_id}`;
-        rowListTitle.innerText = level.name;
         divList.appendChild(rowDiv);
         addEventListenerMe(rowDiv);
     });
-    main.id = 'imgLevelMap'
 }
 
 function addEventListenerMe(wrapper){
