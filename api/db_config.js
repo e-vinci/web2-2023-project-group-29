5b319bf70b3ad6adb16f46f4644fa2a86a3c5dfc
dawid-ciborowski-vinci <dawid.ciborowski@student.vinci.be>
2023-11-26 21:37:50 +0100

    fix : env config

--- api/db_config.js
+++ api/db_config.js
@@ -1,11 +1,15 @@
 const { Client } = require('pg');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const dotenv = require('dotenv');
+
+dotenv.config({ path: 'secrets.env' });
 
 const client = new Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD.toString(),
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = client;
