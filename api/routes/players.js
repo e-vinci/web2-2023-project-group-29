0dac43431a1a57e5fe3abc8ce2da8dcb9f5700de
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2023-12-13 19:50:29 +0100

    test

--- api/routes/players.js
+++ api/routes/players.js
@@ -37,14 +37,14 @@ router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ error: 'Nom d\'utilisateur et mot de passe requis' });
+    return res.status(400).json({ error: 'Email et mot de passe requis' });
   }
 
   try {
     const authenticatedUser = await Player.loginPlayer(email, password);
 
     if (!authenticatedUser) {
-      return res.status(401).json({ error: 'Nom d\'utilisateur ou mot de passe incorrect' });
+      return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
     }
 
     const { token } = authenticatedUser;
