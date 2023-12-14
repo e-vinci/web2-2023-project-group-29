09059b7be3e1b1be986049bdec0c60098b1efc78
dawid-ciborowski-vinci <dawid.ciborowski@student.vinci.be>
2023-12-14 18:47:14 +0100

    add: rank function

--- frontend/src/utils/xp.js
+++ frontend/src/utils/xp.js
@@ -1,5 +1,23 @@
-let xp = 0;
+import { getAuthenticatedUser } from './auths';
 
-async function fetchXp() {
-    
+const { xp } = getAuthenticatedUser();
+
+const rank = calculateRank();
+
+function calculateRank() {
+  const currentLevel = Math.floor(Math.sqrt(xp / 10));
+  const nextLevelPoints = (currentLevel + 1) ** 2 * 10;
+  const progressToNextLevel =
+    ((xp - currentLevel ** 2 * 10) / (nextLevelPoints - currentLevel ** 2 * 10)) * 100;
+
+  return {
+    level: currentLevel,
+    progressPercentage: Math.floor(progressToNextLevel),
+  };
 }
+
+function getPlayerRank() {
+    return rank
+}
+
+export default getPlayerRank();
